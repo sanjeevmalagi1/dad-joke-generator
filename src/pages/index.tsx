490a2b4b5c807c97dc9819c3dfe43707e07856f6
Sanjeev Malagi <sanjeev.malagi@scripbox.com>
2025-01-18 23:41:58 +0530

    Add basic meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 export async function getServerSideProps() {
   const res = await fetch(`${process.env.API_BASE_URL}/api/v1/joke`)
 
@@ -24,11 +26,21 @@ export default function Home(props: any) {
   }
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>{props.joke.setup}</h1>
-        <h2>{props.joke.punchline}</h2>
-      </main>
-    </div>
+    <>
+      <Head>
+        <title>Ultimate Dad Joke Generator</title>
+        <meta name="description" content="Simple dad joke generator that uses OpenAI internally" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="Ultimate Dad Joke Generator" />
+        <meta property="og:description" content="Simple dad joke generator that uses OpenAI internally" />
+        <meta property="og:type" content="website" />
+      </Head>
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+          <h1>{props.joke.setup}</h1>
+          <h2>{props.joke.punchline}</h2>
+        </main>
+      </div>
+    </>
   );
 }
